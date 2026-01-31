80b58452f350d8ab7763a0b372720b3ff28528e3
creativeeyestudio <29424183+tomas-ryan-lee@users.noreply.github.com>
2026-01-31 18:48:20 +0100

    #14 - Masks integration

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -89,13 +89,13 @@ public class GameManager : MonoBehaviour
 
     public void ChangeMask(GameObject newMask, EmotionsList emotion)
     {
-        if (!maskToUpdate && !pnjToUpdate) return;
+        if (!maskToUpdate || !pnjToUpdate) return;
 
         // Sauvegarde des transforms
-        Transform oldTransform = maskToUpdate.transform;
-        Vector3 position = oldTransform.position;
-        Quaternion rotation = oldTransform.rotation;
-        Transform parentTransform = oldTransform.parent;
+        Vector3 position = maskToUpdate.transform.position;
+        Vector3 scale = maskToUpdate.transform.localScale;
+        Quaternion rotation = Quaternion.identity;
+        Transform parentTransform = maskToUpdate.transform.parent;
 
         // Suppression de l'ancien
         Destroy(maskToUpdate.gameObject);
@@ -107,6 +107,8 @@ public class GameManager : MonoBehaviour
             rotation,
             parentTransform
         );
+
+        newMaskInstance.transform.localScale = new Vector3(6f, 6f, 6f);
 
         pnjToUpdate.hasMask = true;
         if (emotion == pnjToUpdate.emotion) {
