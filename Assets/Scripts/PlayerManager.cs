e0373ce44b48c49d5908dfdb111c7eee3cb0a64b
creativeeyestudio <29424183+tomas-ryan-lee@users.noreply.github.com>
2026-02-01 12:13:27 +0100

    #22 - Camera config

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -1,3 +1,4 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
@@ -7,6 +8,14 @@ public class PlayerManager : MonoBehaviour
     [SerializeField] private GameObject _player;
     [SerializeField] private float _speed;
     private CharacterController _characterController;
+
+    [Header("Camera Settings")]
+    [SerializeField] private float _distance = 5f;
+    [SerializeField] private float _sensitivity = 3f;
+    [SerializeField] private float _minY = -30f;
+    [SerializeField] private float _maxY = 60f;
+
+    float rotationX, rotationY;
 
     private void Awake()
     {
@@ -19,18 +28,39 @@ public class PlayerManager : MonoBehaviour
         Instance = this;
     }
 
-    private void Start() 
+    private void Start()
     {
         _characterController = _player.GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
-    private void Update() {
+    private void Update()
+    {
         Move();
+    }
+
+    private void LateUpdate()
+    {
+        MoveCamera()
     }
 
     private void Move()
     {
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         _characterController.Move(move * Time.deltaTime * _speed);
+    }
+
+    private void MoveCamera()
+    {
+        rotationX += Input.GetAxis("Mouse X") * sensitivity;
+        rotationY -= Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY = Mathf.Clamp(rotationY, minY, maxY);
+
+        Quaternion rotation = Quaternion.Euler(rotationY, rotationX, 0);
+        Vector3 position = target.position - (rotation * Vector3.forward * distance);
+
+        transform.position = position;
+        transform.rotation = rotation;
     }
 }
