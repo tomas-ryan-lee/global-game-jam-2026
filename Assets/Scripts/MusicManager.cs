0f666f7f443913f1352977133d63402cde80af87
creativeeyestudio <29424183+tomas-ryan-lee@users.noreply.github.com>
2026-02-01 08:36:19 +0100

    #4 - Music Manager Sources

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class MusicManager : MonoBehaviour
 {
+    [SerializeField] private AudioSource _audioSource;
+
+    [Header("Mask musics")]
+    [SerializeField] private AudioClip _joySound;
+    [SerializeField] private AudioClip _sadSound;
+    [SerializeField] private AudioClip _wrathSound;
+    [SerializeField] private AudioClip _fearSound;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
