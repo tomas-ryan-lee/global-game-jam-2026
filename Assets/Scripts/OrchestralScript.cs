e1b0cabd222468a4b5afe29fffcf5c71571c0370
Mickael-95 <72389929+Mickael-95@users.noreply.github.com>
2026-02-01 15:16:29 +0100

    Update level design scene

--- Assets/Scripts/OrchestralScript.cs
+++ Assets/Scripts/OrchestralScript.cs
@@ -3,7 +3,14 @@ using UnityEngine;
 public class OrchestralScript : MonoBehaviour
 {
     [SerializeField][TextAreaAttribute] string _dialogText;
-    
+    [SerializeField] private Material SkyboxHappy;
+    [SerializeField] private GameObject vfxMusicalNotesPNJ1;
+    [SerializeField] private GameObject vfxMusicalNotesPNJ2;
+    [SerializeField] private GameObject vfxMusicalNotesPNJ3;
+    [SerializeField] private GameObject vfxRain;
+    [SerializeField] private GameObject vfxFireworks;
+    [SerializeField] private GameObject joyMask;
+
     void CheckAllPNJHaveGoodMask()
     {
         GameObject[] pnjs = GameObject.FindGameObjectsWithTag("PNJ");
@@ -12,11 +19,28 @@ public class OrchestralScript : MonoBehaviour
         {
             PNJScript script = pnj.GetComponent<PNJScript>();
 
-            if (script == null || !script.hasMask || !script.hasGoodMask) return;
+            if (script == null || !script.hasMask || !script.hasGoodMask)
+                GameManager.Instance.maskToUpdate = joyMask;
+                GameManager.Instance.pnjToUpdate = script;
+                vfxMusicalNotesPNJ1.SetActive(true);
+            vfxMusicalNotesPNJ2.SetActive(true);
+            vfxMusicalNotesPNJ3.SetActive(true);
+            vfxRain.SetActive(false);
+            vfxFireworks.SetActive(true);
+            return;
         }
 
         GameManager.Instance.canOpenDialogOrchestral = true;
         GameManager.Instance.dialogText = _dialogText;
+    }
+
+    public void ChangeSkybox()
+    {
+        if (SkyboxHappy != null)
+        {
+            RenderSettings.skybox = SkyboxHappy;
+            DynamicGI.UpdateEnvironment();
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -24,6 +48,7 @@ public class OrchestralScript : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             CheckAllPNJHaveGoodMask();
+            ChangeSkybox();
         }
     }
 
